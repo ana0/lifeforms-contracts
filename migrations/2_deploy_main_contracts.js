4adf61f6bbdf7eb1c09ea294ee3a4583cae4d441
ana0 <sarah.anne.friend@gmail.com>
2021-08-18 16:14:01 +0200

    Migrate with libraries

---
+++ migrations/2_deploy_main_contracts.js
@@ -0,0 +1,22 @@
+const truffleContract = require('@truffle/contract');
+
+const Lifeforms = artifacts.require('Lifeforms');
+const EnumerableSetArtifacts = require('@openzeppelin/contracts/build/contracts/EnumerableSet.json');
+const EnumerableMapArtifacts = require('@openzeppelin/contracts/build/contracts/EnumerableMap.json');
+
+const EnumerableSet = truffleContract(EnumerableSetArtifacts);
+EnumerableSet.setProvider(web3.currentProvider);
+const EnumerableMap = truffleContract(EnumerableMapArtifacts);
+EnumerableMap.setProvider(web3.currentProvider);
+
+module.exports = async function (deployer, network, accounts) {
+  await deployer.deploy(EnumerableSet, { from: accounts[0] });
+  await deployer.deploy(EnumerableMap, { from: accounts[0] });
+  await deployer.link(EnumerableSet, Lifeforms);
+  await deployer.link(EnumerableMap, Lifeforms);
+  return deployer.deploy(Lifeforms, 'Lifeforms', 'LIFE', 60, web3.utils.toWei('10'), { from: accounts[0] })
+    .then(async (lifeforms) => {
+      await lifeforms.setBaseURI('https://isthisa.computer/ipfs/');
+      await lifeforms.setContractURI('https://isthisa.computer/ipfs/QmXquYVrt7pvJ6yAZq4BtKnbjwoagpfbpwn6MMeCYnq8JM/off-contract.json');
+    });
+};
